4e88fe919098892fd2f8f96b40ba44b03d0a1407
hectorkutleng <hectorh@kutleng.co.za>
2019-03-07 22:40:15 +0200

    Added verbose error return

--- casperbsp/partialreconfig/xilinxbitstream.py
+++ casperbsp/partialreconfig/xilinxbitstream.py
@@ -67,24 +67,57 @@ prloaderlib = CDLL("./libxilinxbitstream.so")
 
 def main():
 	#Send the bitfile over UDP calling the C library function
-	retval = c_int(prloaderlib.configurepartialbitfile(20000,"192.168.100.10", "./bitfiles/vcu1525/PartialBlinker_i_partialblinker_partial.bin"))
+	retval = c_int(prloaderlib.configurepartialbitfile(20000,"192.168.100.10", "./bitfiles/vcu1525/PartialBlinker_i_partialflasher_partial.bin"))
 	print("STAT Register is = %08x" % retval.value)
+	print("Mode pins M[2:0]=%0x" %((retval.value>>8)&0x00000007))
+	print("CFG_STARTUP_PHASE="+str((retval.value>>18)&0x00000007))
+	print("CFG_BUS_WIDTH="+str((retval.value>>25)&0x00000003))
 	#Check the CRC Error bit
 	if retval.value&0x00000001:
-		print("CRC Error after config")
+		print("CRC_ERROR = 1,CRC Error after config")
 	else:
-		print("No CRC Error after config")
+		print("CRC_ERROR = 0,No CRC Error after config")
+	#Check the Decryptor bit
+	if retval.value&0x00000002:
+		print("DECRYPTOR_ENABLED = 1,Security set")
+	else:
+		print("DECRYPTOR_ENABLED = 0,Security not set")
+	#Check the PLL lock bit
+	if retval.value&0x00000004:
+		print("MMCM_PLL_LOCKED = 1,PLLs are locked")
+	else:
+		print("MMCM_PLL_LOCKED = 0,PLLs are not locked")
+	#Check the DCI Match bit
+	if retval.value&0x00000008:
+		print("DCI_MATCH_STATUS = 1,DCI has matched")
+	else:
+		print("DCI_MATCH_STATUS = 0,DCI has not matched")
 	#Check the EOS bit
 	if retval.value&0x00000010:
 		print("EOS Status:1 = Finished")
 	else:
 		print("EOS Status:0 = Not finished")
-	print("Mode pins M[2:0]=%0x" %((retval.value>>8)&0x00000007))
-	print("CFG_STARTUP_PHASE="+str((retval.value>>18)&0x00000007))
-	print("CFG_BUS_WIDTH="+str((retval.value>>25)&0x00000003))
+	#Check the CFG_B bit
+	if retval.value&0x00000020:
+		print("GTS_CFG_B_STATUS = 1 all I/Os configured")
+	else:
+		print("GTS_CFG_B_STATUS = 0 all I/Os in highZ")
+	#Check the GWE bit
+	if retval.value&0x00000040:
+		print("GWE_STATUS = 1 all FF/RAM are write enabled")
+	else:
+		print("GWE_STATUS = 0 all FF/RAM are write disabled")
+	#Check the GHIGH bit
+	if retval.value&0x00000080:
+		print("GHIGH_B_STATUS = 1 deasserted")
+	else:
+		print("GHIGH_B_STATUS = 0 asserted")
+		
 	if retval.value&0x00000800:
+		print("INIT_B_INTERNAL_SIGNAL_STATUS = 1")
 		print("Inititalisation finished")
 	else:
+		print("INIT_B_INTERNAL_SIGNAL_STATUS = 0")
 		print("Initialisation not finished")
 	if retval.value&0x00001000:
 		print("INIT_B:1 High")
